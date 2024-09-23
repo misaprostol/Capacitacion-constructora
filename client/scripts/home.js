1f54a1ebb71d9e82a2b2ab739990654596e89f5d
martin <164651909+lenrit@users.noreply.github.com>
2024-09-23 17:21:50 -0300

    Solucione lo de aumentar y disminuir fuente en las cards

--- client/scripts/home.js
+++ client/scripts/home.js
@@ -1,4 +1,6 @@
 const bodyElement = document.body;
+const parrafos=document.querySelectorAll("p");
+console.log(parrafos);
 let currentFontSize = 16; // Tamaño de fuente inicial en píxeles
 
 // Función para aumentar el tamaño de la fuente
@@ -6,6 +8,10 @@ function increaseFontSize() {
     if (currentFontSize < 30) { // Límite máximo de 30px
     currentFontSize += 2;
     bodyElement.style.fontSize = currentFontSize + 'px';
+    parrafos[0].style.fontSize = currentFontSize + 'px';
+    parrafos[1].style.fontSize = currentFontSize + 'px';
+    parrafos[2].style.fontSize = currentFontSize + 'px';
+    parrafos[3].style.fontSize = currentFontSize + 'px';
     }
 }
 
@@ -14,6 +20,10 @@ function decreaseFontSize() {
     if (currentFontSize > 12) { // Límite mínimo de 12px
     currentFontSize -= 2;
     bodyElement.style.fontSize = currentFontSize + 'px';
+    parrafos[0].style.fontSize = currentFontSize + 'px';
+    parrafos[1].style.fontSize = currentFontSize + 'px';
+    parrafos[2].style.fontSize = currentFontSize + 'px';
+    parrafos[3].style.fontSize = currentFontSize + 'px';
     }
 }
 
